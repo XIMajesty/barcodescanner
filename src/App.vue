8c7bcc1406cd249b5f04e16384b32371daf525fa
WilkaXDD <wilka.b@gmx.de>
2023-10-13 16:39:59 +0200

    test

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <template>
   <header>Barcode-Scanner</header>
-  <button @click="toggleText">Scan</button>
+  <button @click="toggleKamera">Scan</button>
   <p v-if="successTest">  Gescannt</p>
   <StreamBarcodeReader
     @decode="onDecode"
     @loaded="onLoaded"
     v-if="successTest"
   ></StreamBarcodeReader>
+  <p>{{ decodedText }}</p>
 </template>
 
 <script>
@@ -14,7 +15,8 @@ import { StreamBarcodeReader } from "vue-barcode-reader";
 export default {
   data() {
     return {
-      successTest: false
+      decodedText: null,
+      successTest: false,
     };
   },
   components: {
@@ -23,11 +25,12 @@ export default {
   methods: {
     onDecode(text) {
       console.log(`Decode text from QR code is ${text}`)
+      decodedText = text
     },
     onLoaded() {
       console.log(`Ready to start scanning barcodes`)
     },
-    toggleText() {
+    toggleKamera() {
       this.successTest = !this.successTest;
     }
   }
