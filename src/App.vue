5533660f190867ab8a1d8691c6c2d77da678d671
Coelt <jonasm.sauer@web.de>
2023-10-14 01:12:25 +0200

    kleine Style änderung nur um wieder rein zu kommen

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
-  <header>Barcode-Scanner</header>
-  <button @click="toggleKamera">Start Scan</button>
-  <p v-if="successTest">  Gescannt</p>
-  <StreamBarcodeReader
-    @decode="onDecode"
-    @loaded="onLoaded"
-    v-if="successTest"
-  ></StreamBarcodeReader>
-  <p>{{ decodedText }}</p>
-  <img src="https://img.offers-cdn.net/assets/uploads/offers/de/20595789/limetto-cola-mix-o-cola-mix-zero-20-500-ml-large.jpeg" alt="Cola-Mix Limonade" v-if="decodedText == 42261322">
+  <header>
+    <h1 class="center">Barcode-Scanner</h1>
+  </header>
+  <body id="body">
+    <section id="firstSection">
+      <button @click="toggleKamera" class="center">Start Scan</button>
+    <p v-if="successTest" class="center">Gescannt</p>
+    <StreamBarcodeReader @decode="onDecode" @loaded="onLoaded" v-if="successTest"></StreamBarcodeReader>
+    <p class="center">{{ decodedText }}</p>
+    <img src="https://img.offers-cdn.net/assets/uploads/offers/de/20595789/limetto-cola-mix-o-cola-mix-zero-20-500-ml-large.jpeg" alt="Cola-Mix Limonade" v-if="decodedText == 42261322">
+    </section>
+    <section id="secondSection">
+
+    </section>
+  </body>
 </template>
 
 <script>
@@ -28,7 +33,6 @@ export default {
       console.log(`Decode text from QR code is ${text}`)
       this.decodedText = text
       this.successTest = !this.successTest;
-
     },
     onLoaded() {
       console.log(`Ready to start scanning barcodes`)
@@ -39,3 +43,19 @@ export default {
   }
 };
 </script>
+
+<style>
+  #body{
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: 80%;
+  }
+
+  .center{
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    margin-left: auto;
+    margin-right: auto;
+  }
+</style>
