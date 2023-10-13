bcce987782e764cc0b5385c60e4be2826e7177fd
WilkaXDD <wilka.b@gmx.de>
2023-10-13 16:05:54 +0200

    Scanner implementierung

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,35 @@
 <template>
-  <div>
-    <button @click="startScanning">Start Scan</button>
-    <p v-if="decodedText">Decoded text: {{ decodedText }}</p>
-    <stream-barcode-reader v-if="scannerEnabled" @newCode="onDecode"></stream-barcode-reader>
-  </div>
+  <header>Barcode-Scanner</header>
+  <button @click="toggleText">Scan</button>
+  <p v-if="successTest">  Gescannt</p>
+  <StreamBarcodeReader
+    @decode="onDecode"
+    @loaded="onLoaded"
+    v-if="successTest"
+  ></StreamBarcodeReader>
 </template>
 
 <script>
 import { StreamBarcodeReader } from "vue-barcode-reader";
-
 export default {
   data() {
     return {
-      decodedText: null,
-      scannerEnabled: false,
+      successTest: false
     };
   },
   components: {
     StreamBarcodeReader,
   },
   methods: {
-    onDecode(code) {
-      this.decodedText = code;
+    onDecode(text) {
+      console.log(`Decode text from QR code is ${text}`)
     },
-    startScanning() {
-      this.scannerEnabled = !this.scannerEnabled;
+    onLoaded() {
+      console.log(`Ready to start scanning barcodes`)
     },
-  },
+    toggleText() {
+      this.successTest = !this.successTest;
+    }
+  }
 };
 </script>
