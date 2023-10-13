59164f4d7d7e16b59ad37dc96200a429eaeeb149
Coelt <jonasm.sauer@web.de>
2023-10-13 17:14:16 +0200

    Bild Test

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
     v-if="successTest"
   ></StreamBarcodeReader>
   <p>{{ decodedText }}</p>
-  <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.prospektangebote.de%2Fgeschaefte%2Fnetto-getraenke-discount%2Fangebote%2Flimetto-cola-mix-o-cola-mix-zero-20-500-ml-angebot-20595789%2F&psig=AOvVaw00tjwK0Q0lAh_MJiVuOXjP&ust=1697296085067000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCIjt7uqm84EDFQAAAAAdAAAAABAD" alt="Cola-Mix Limonade" v-if="decodedText == 42261322">
+  <img src="https://img.offers-cdn.net/assets/uploads/offers/de/20595789/limetto-cola-mix-o-cola-mix-zero-20-500-ml-large.jpeg" alt="Cola-Mix Limonade" v-if="decodedText == 42261322">
 </template>
 
 <script>
@@ -27,6 +27,8 @@ export default {
     onDecode(text) {
       console.log(`Decode text from QR code is ${text}`)
       this.decodedText = text
+      this.successTest = !this.successTest;
+
     },
     onLoaded() {
       console.log(`Ready to start scanning barcodes`)
