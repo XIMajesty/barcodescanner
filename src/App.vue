312cb792d2cd2cce04b56df4a26285f0e7e86aef
Coelt <jonasm.sauer@web.de>
2023-10-13 17:09:23 +0200

    Bild Test

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
     v-if="successTest"
   ></StreamBarcodeReader>
   <p>{{ decodedText }}</p>
+  <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.prospektangebote.de%2Fgeschaefte%2Fnetto-getraenke-discount%2Fangebote%2Flimetto-cola-mix-o-cola-mix-zero-20-500-ml-angebot-20595789%2F&psig=AOvVaw00tjwK0Q0lAh_MJiVuOXjP&ust=1697296085067000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCIjt7uqm84EDFQAAAAAdAAAAABAD" alt="Cola-Mix Limonade" v-if="decodedText == 42261322">
 </template>
 
 <script>
