45797fbd60e9149787064f2060205cc196baa529
XIMajesty <136703549+XIMajesty@users.noreply.github.com>
2023-10-14 10:08:07 +0200

    design changes

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <template>
   <header>Barcode-Scanner</header>
   <button @click="toggleKamera">Start Scan</button>
-  <p v-if="successTest">  Gescannt</p>
-  <StreamBarcodeReader
-    @decode="onDecode"
-    @loaded="onLoaded"
-    v-if="successTest"
-  ></StreamBarcodeReader>
+  <p v-if="successTest"> Scanning</p>
+  <StreamBarcodeReader @decode="onDecode" @loaded="onLoaded" v-if="successTest"></StreamBarcodeReader>
   <p>{{ decodedText }}</p>
-  <img src="https://img.offers-cdn.net/assets/uploads/offers/de/20595789/limetto-cola-mix-o-cola-mix-zero-20-500-ml-large.jpeg" alt="Cola-Mix Limonade" v-if="decodedText == 42261322">
+  <img
+    src="https://img.offers-cdn.net/assets/uploads/offers/de/20595789/limetto-cola-mix-o-cola-mix-zero-20-500-ml-large.jpeg"
+    alt="Cola-Mix Limonade" v-if="decodedText == 42261322">
 </template>
 
 <script>
@@ -39,3 +37,87 @@ export default {
   }
 };
 </script>
+
+<style>
+/* Global styles */
+body {
+  font-family: 'Arial', sans-serif;
+  margin: 0;
+  padding: 0;
+  background-color: #212020;
+}
+
+header {
+  background-color: #333;
+  color: #fff;
+  text-align: center;
+  padding: 10px 0;
+  font-size: 24px;
+  margin-bottom: 20px;
+  border-radius: 5px;
+}
+
+button {
+  display: block;
+  background-color: #007BFF;
+  color: #fff;
+  text-align: center;
+  border: none;
+  border-radius: 5px;
+  padding: 15px;
+  width: 100%;
+  cursor: pointer;
+  font-size: 18px;
+  margin-bottom: 20px;
+}
+
+button:active {
+  background-color: #0056b4;
+}
+
+p {
+  text-align: center;
+  padding: 10px;
+  background-color: #eee;
+  border-radius: 5px;
+  font-size: 16px;
+}
+
+img {
+  max-width: 100%;
+  /* Makes sure the image doesn't overflow its parent container */
+  display: block;
+  margin: 15px auto;
+  border-radius: 5px;
+}
+
+/* Responsive design adjustments */
+
+/* For tablets or smaller screens */
+@media (max-width: 768px) {
+  header {
+    font-size: 20px;
+  }
+
+  button {
+    padding: 12px;
+    font-size: 16px;
+  }
+}
+
+/* For smartphones or very small screens */
+@media (max-width: 480px) {
+  header {
+    font-size: 18px;
+  }
+
+  button {
+    padding: 10px;
+    font-size: 14px;
+  }
+
+  p {
+    font-size: 14px;
+  }
+}
+</style>
